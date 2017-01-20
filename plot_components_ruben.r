46e97383540a8c01e9d21c49984ee706a4e89f56
Ruben C. Arslan <rubenarslan@gmail.com>
2017-01-20 19:07:52 +0100

    update

--- plot_components_ruben.r
+++ plot_components_ruben.r
@@ -71,23 +71,6 @@ x %>% group_by(Trait) %>%
 	summarise(Variance = sum(Variance)) %>% mutate(Source = "Gen")
 
 
-#
-# barwidth = 0.7
-# stack_dodge = ggplot(z,aes(x = Phenotypes, y = Variance,fill = Source)) +
-# 	geom_col(width = barwidth, alpha = 0.7) +
-# 	geom_linerange(aes(ymin = Variance - Standard.Error, ymax = Variance + Standard.Error),
-# 								 position = position_dodge(width = barwidth),stat = "identity") +
-# 	facet_wrap(~ Trait, strip.position = "left", ncol = 1) +
-# 	theme_bw() +
-# 	blankfacets +
-# 	ggtitle("Genetic and environmental contributions to phenotypic variance")+
-# 	scale_fill_manual("Variance components", values=c("Common genetic" = "red2", "Pedigree genetic" = "red4", "Family" = "blue4", "Sibling" = "lightskyblue",  "Couple" = "blue2", "Total" = "black"))+
-# 	scale_y_continuous("Variance explained %", breaks=c(0,10,20,30,40,50,60,70,80,90,100))+
-# 	scale_x_discrete()+
-# 	coord_flip()
-
-
-
 
 
 #### marioni
@@ -139,67 +122,3 @@ comp_pedi = ggplot(marioni,aes(x = Phenotypes, y = Variance,fill = Source)) +
 comp_pedi
 
 ggsave(comp_pedi, filename = "comp_to_marioni.pdf", width = 17.8, height = 17.8*0.625)
-
-
-
-#
-# stack_dodge + scale_fill_brewer("Phenotypes", palette = 1)
-# stack_dodge + scale_fill_manual("Phenotypes", values=
-# 		c(
-# 			"Common genetic" = "black",
-# 			"Pedigree genetic" = "#9F3B2B",
-# 			"Family" = "#8A846C",
-# 			"Sibling" = "#21523B",
-# 			"Couple" = "#E2A674"
-# 		)
-# )
-#
-# ggplot(x,aes(x = Trait, y = Variance,fill = Source)) +
-#     geom_bar(position = "stack",stat = "identity",width =0.5) +
-# 	theme_bw() + theme(axis.line.x = element_line(colour = "black"),axis.line.y = element_line(colour = "black"),  panel.border = element_blank(), panel.grid.major = element_blank(),
-# panel.grid.minor = element_blank(),
-#         axis.title.x = element_text(face="bold", colour='black', size=28),
-#         axis.text.x  = element_text(size=28, colour='black'),
-#         axis.title.y = element_text(face="bold", colour='black', size=28),
-#         axis.text.y  = element_text(size=28, colour='black'),
-#         legend.position=c(0.9, 0.8),
-#         legend.title = element_text(colour="black", size=30),
-#         legend.text = element_text(colour="black", size =30),
-#         plot.title = element_text(face="bold", size=30))+
-# 		theme(axis.text.x  = element_text(angle=45, vjust=1, hjust = 1, size=30))+
-# 		theme(axis.text.y  = element_text(vjust=1, hjust = 1, size=30))+
-# 		ggtitle("Genetic and environmental contributions to phenotypic variance")+
-# 		labs(x="Phenotypes", y="Variance explained %")+
-# 	scale_fill_manual(values=c("Common genetic" = "red2", "Pedigree genetic" = "red4", "Family" = "blue4", "Sibling" = "lightskyblue",  "Couple" = "blue2"))+
-# 	scale_y_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100))+
-# 	scale_x_discrete()+
-# 	coord_cartesian(ylim=c(0,100))
-
-# x$Source = factor(x$Source, levels = rev(c("Common genetic","Pedigree genetic","Family","Sibling","Couple")))
-# y = x %>% bind_rows(x %>% group_by(Trait) %>%
-# 	summarise(Variance = sum(Variance)) %>% mutate(Source = "Total"))
-# y$Source = factor(y$Source, levels = c("Common genetic","Pedigree genetic","Family","Sibling","Couple", "Total"))
-
-# barwidth = 0.7
-# ggplot(y,aes(x = Trait, y = Variance,fill = Source)) +
-# 	geom_bar(position = position_dodge(width = barwidth),stat = "identity", width =barwidth) +
-# 	geom_linerange(aes(ymin = Variance - Standard.Error, ymax = Variance + Standard.Error), position = position_dodge(width = barwidth),stat = "identity") +
-# 	theme_bw() + theme(axis.line.x = element_line(colour = "black"),axis.line.y = element_line(colour = "black"),  panel.border = element_blank(), panel.grid.major.y = element_blank(),
-# 										 panel.grid.minor.y = element_blank(),
-# 										 axis.title.x = element_text(face="bold", colour='black', size=25),
-# 										 axis.text.x  = element_text(size=28, colour='black'),
-# 										 axis.title.y = element_text(face="bold", colour='black', size=25),
-# 										 axis.text.y  = element_text(size=28, colour='black'),
-# 										 legend.position=c(0.8, 0.2),
-# 										 legend.title = element_text(colour="black", size=25),
-# 										 legend.text = element_text(colour="black", size =25),
-# 										 plot.title = element_text(face="bold", size=25),
-# 										 legend.key.size = unit(0.8,"cm"))+
-# 	ggtitle("Genetic and environmental contributions to phenotypic variance")+
-# 	labs(x="Phenotypes", y="Variance explained %")+
-# 	scale_fill_manual(values=c("Common genetic" = "red2", "Pedigree genetic" = "red4", "Family" = "blue4", "Sibling" = "lightskyblue",  "Couple" = "blue2", "Total" = "black"))+
-# 	scale_y_continuous(breaks=c(0,10,20,30,40,50,60,70,80,90,100))+
-# 	scale_x_discrete()+
-# 	coord_flip(ylim = c(0,100))
-#
-# ggsave(filename = "ind_components.pdf", width = 15, height = 15)
